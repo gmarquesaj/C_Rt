4a0f6a56ebcfe985b9c3b26caa3bf77203026521
Gilson Marques Alves Junior <gmarquesaj@gmail.comr>
2022-12-12 18:11:26 -0300

    adicionado estruturas

---
+++ main.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+typedef struct
+{
+ float x,y,z;
+}vec3;
+
+typedef struct
+{
+ vec3 v1,v2,v3,n1,n2,n3;
+}tri;
+typedef struct 
+{
+	const int w,h;
+	float *pixels;
+} ppm;
+int randi()
+{
+return 0;
+};
+float randf()
+{
+	return 0.0f;
+}
+int randi_r(int mi,int ma)
+{
+return 0;
+};
+float randf_r(float mi, float ma)
+{
+	return 0.0f;
+};
+float randf_hem()
+{
+return 0.0f;
+};
+
+int main()
+{
+	printf("ola mundo\n");
+return 0;
+}
